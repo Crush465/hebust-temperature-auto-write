7504458b197c88b1579ff9e7e311dc4ad1cb6fb2
Crush465 <82747197+Crush465@users.noreply.github.com>
2022-03-24 16:13:41 +0800

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -5,129 +5,139 @@ import smtplib
 import random
 from email.mime.text import MIMEText
 from email.header import Header
-from time import sleep
-a = random.uniform(36.3,36.7)
-a= str(a)[:4:]
-#设填写的体温a是36.3~36.7的随机数
-my_email = "???????????@qq.com" #此处是python发送邮件利用的邮箱
-send_mail = "?????????????@qq.com" #此处是接收填写情况的邮箱
-license_code = "???????????" #发送邮箱的pop授权码
-smtp_server = "smtp.qq.com"  # qq smtp 的服务器
-#此处是找到当前时间戳定位url
-timetamp = time.mktime(time.localtime())
-timetamp = int(timetamp)
-#各个链接
-url="http://xscfw.hebust.edu.cn/survey/ajaxLogin" #登录链接
-url2="http://xscfw.hebust.edu.cn/survey/index"   #获取sid链接
-url3=f"http://xscfw.hebust.edu.cn/survey/surveySave?timestamp={timetamp}" #填报体温的地址
-def send_email(content):
-    #发送邮件函数
-    try:
-        if content == "未完成":
-            msg = MIMEText(f"你尚未完成填写+你将要填写的体温是{a}","plain","utf-8")
-        else:
-            msg = MIMEText(f"你已完成填写，无须再填写", "plain", "utf-8")
-        #判断填写情况并发送邮件
-        msg['From'] = Header(my_email)
-        msg['To'] = Header(send_mail)
-        msg['Subject'] = Header("健康填报情况")
-        server = smtplib.SMTP_SSL(host=smtp_server)
-        server.connect(smtp_server,465)
-        server.login(my_email,password=license_code)
-        server.sendmail(my_email,send_mail,msg.as_string())
-        server.quit()
-        #连接服务器
-    except:
-        print('error')
-        #程序运行失败的报错信息
-def tianbao():
-    #填报程序
-    try:
-        timetamp = time.mktime(time.localtime())
-        timetamp = int(timetamp)
-        rep= r.post(url=url3,params=data,headers=header,cookies=cookies)
-        print("填报成功")
-    except:
-        print("填报出错")
 
-#账号信息
-param={
-    "stuNum": "?????????",#学号
-    "pwd": "??????????",#密码
-    "vcode": "",
-}
-#
-header={
-"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62"
-}
-while True:
-    #登录程序
-    try:
-        response=r.post(url=url,params=param,headers=header)
-        cookiesJAR = response.cookies#获取cookies
-        cookies = cookiesJAR.get_dict()#把cookies写成字典形式
-        res = r.get(url=url2,headers=header,cookies=cookies,params=param)
-        print("登录成功")
-    except:
-        print("登录失败")
-    #获取完成情况
-    try:
-        res.encoding = 'uft-8'
-        html = etree.HTML(res.text)
-        content = html.xpath('/html/body/ul/li[1]/div/span/text()')
-    except:
-        print("获取失败")
-    #获取当前日期要填的文档的sid
-    try:
-        url4 = 'http://xscfw.hebust.edu.cn/survey/index.action'
-        rek =r.get(url=url4,cookies=cookies,headers=header)
-        rek.encoding = 'utf-8'
-        html3 =etree.HTML(rek.text)
-        sid = html3.xpath('/html/body/ul/li[1]/@sid')[0]
-        print(f"获取sid成功：{sid}")
-    except:
-        print("获取sid失败")
-    #####获取stuId和qid
-    try:
-        url5 = f'http://xscfw.hebust.edu.cn/survey/surveyEdit?id={sid}'
-        rej = r.get(url=url5,cookies=cookies,headers=header)
-        rej.encoding = 'utf-8'
-        html2 =etree.HTML(rej.text)
-        stuId = html2.xpath('//*[@id="surveyForm"]/input[2]/@value')[0]
-        qid = html2.xpath('//*[@id="surveyForm"]/input[3]/@value')[0]
-        print(f"获取stuId成功：{stuId}")
-        print(f"获取qid成功:{qid}")
-    except:
-        print("获取stuId qid 失败")
-    #####要填写的数据,其中a是36.3~36.7的随机数
-    try:
-        data={
-        "id":sid,
-        "stuId":stuId,
-        "qid":qid,
-        "location":'',
-        "c0":"不超过37.3℃，正常",
-        "c1":a,
-        "c3":"不超过37.3℃，正常",
-        "c4":a,
-        "c6":"健康",
+# 此段代码用于个人测试用。
+# 在__init__方法内修改信息即可使用
+# 想全天监控填写情况建议把此段代码放在阿里云的函数计算上面，并用触发器设置执行时间
+
+class AuotoWrite:
+    # 加载所有信息
+    def __init__(self):
+
+        # ?处是要填写的
+        self.data = None
+        self.param = {
+            "stuNum": "?",  # 学号
+            "pwd": "?",  # 密码
+            "vcode": "",
         }
-    except:
-        print("获取信息有误")
-    #判断程序
-    try:
-        now_time=time.strftime("%H%M%S",time.localtime())
-        now_time = int(now_time)
-        if 100000 <= now_time <= 170000:
-            print("符合时间要求，开始执行")
+        self.my_email = "?"  # 此处是python发送邮件利用的邮箱
+        self.send_mail = "?"  # 此处是接收填写情况的邮箱
+        self.license_code = "?"  # 发送邮箱的pop授权码
+        self.smtp_server = "smtp.qq.com"  # qq smtp 的服务器
+        # 以下已设置好
+        # 设填写的体温a是36.3~36.7的随机数
+        # 此处是找到当前时间戳定位url
+        self.a = random.uniform(36.3, 36.7)
+        self.a = str(self.a)[:4:]
+        self.timetamp = time.mktime(time.localtime())
+        self.timetamp = int(self.timetamp)
+
+        # header参数，防止检测到非人类
+        self.header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.62"
+        }
+        # 各个链接
+        self.url = "http://xscfw.hebust.edu.cn/survey/ajaxLogin"  # 登录链接
+        self.url2 = "http://xscfw.hebust.edu.cn/survey/index"  # 获取sid链接
+        self.url3 = f"http://xscfw.hebust.edu.cn/survey/surveySave?timestamp={self.timetamp}"  # 填报体温的地址
+
+    def send_email(self, content):
+        # 发送邮件函数
+        try:
+
+            if content == "未完成":
+                msg = MIMEText(f"你尚未完成填写+你将要填写的体温是{self.a}", "plain", "utf-8")
+            else:
+                msg = MIMEText(f"你已完成填写，无须再填写", "plain", "utf-8")
+            # 判断填写情况并发送邮件
+            msg['From'] = Header(self.my_email)
+            msg['To'] = Header(self.send_mail)
+            msg['Subject'] = Header("健康填报情况")
+            server = smtplib.SMTP_SSL(host=self.smtp_server)
+            server.connect(self.smtp_server, 465)
+            server.login(self.my_email, password=self.license_code)
+            server.sendmail(self.my_email, self.send_mail, msg.as_string())
+            server.quit()
+            print("填写情况是" +self.a)
+            # 连接服务器
+        except:
+            print('邮件发送失败')
+            # 程序运行失败的报错信息
+
+    def tianbao(self):
+        # 填报程序
+        try:
+            rep = r.post(url=self.url3, params=self.data, headers=self.header, cookies=self.cookies)
+            print(rep)
+        except:
+            print("填报出错")
+
+    # 账号信息
+    def start(self):
+        try:
+            response = r.post(url=self.url, params=self.param, headers=self.header)
+            cookiesJAR = response.cookies  # 获取cookies
+            cookies = cookiesJAR.get_dict()  # 把cookies写成字典形式
+            res = r.get(url=self.url2, headers=self.header, cookies=cookies, params=self.param)
+            print("登录成功")
+        except:
+            print("登录失败")
+        # 获取完成情况
+        try:
+            res.encoding = 'uft-8'
+            html = etree.HTML(res.text)
+            content = html.xpath('/html/body/ul/li[1]/div/span/text()')
+        except:
+            print("获取失败")
+        # 获取当前日期要填的文档的sid
+        try:
+            url4 = 'http://xscfw.hebust.edu.cn/survey/index.action'
+            rek = r.get(url=url4, cookies=cookies, headers=self.header)
+            rek.encoding = 'utf-8'
+            html3 = etree.HTML(rek.text)
+            sid = html3.xpath('/html/body/ul/li[1]/@sid')[0]
+            print(f"获取sid成功：{sid}")
+        except:
+            print("获取sid失败,请检查你的学号和密码是否填写正确")
+        #####获取stuId和qid
+        try:
+            url5 = f'http://xscfw.hebust.edu.cn/survey/surveyEdit?id={sid}'
+            rej = r.get(url=url5, cookies=cookies, headers=self.header)
+            rej.encoding = 'utf-8'
+            html2 = etree.HTML(rej.text)
+            stuId = html2.xpath('//*[@id="surveyForm"]/input[2]/@value')[0]
+            qid = html2.xpath('//*[@id="surveyForm"]/input[3]/@value')[0]
+            print(f"获取stuId成功：{stuId}")
+            print(f"获取qid成功:{qid}")
+        except:
+            print("获取stuId qid 失败")
+        #####要填写的数据,其中a是36.3~36.7的随机数
+        try:
+            self.data = {
+                "id": sid,
+                "stuId": stuId,
+                "qid": qid,
+                "location": '',
+                "c0": "不超过37.3℃，正常",
+                "c1": self.a,
+                "c3": "不超过37.3℃，正常",
+                "c4": self.a,
+                "c6": "健康",
+            }
+        except:
+            print("获取信息有误")
+        # 判断程序
+        try:
+            print("开始执行")
             if content[0] == '已完成':
-                send_email('已完成')
-                sleep(60)
-            if content[0] =='未完成':
-                    send_email('未完成')
-                    tianbao()
-                    sleep(60)
-        else:
-            print("填写时间未到")
-    except:
-        print("判断失败")
+                self.send_email('已完成')
+            if content[0] == '未完成':
+                self.send_email('未完成')
+                print("填写开始")
+                self.tianbao()
+            print("已发送填写情况邮件")
+        except:
+            print("判断填写情况失败")
+A = AuotoWrite()
+A.start()
